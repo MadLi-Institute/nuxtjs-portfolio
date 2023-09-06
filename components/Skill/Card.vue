66f39a14e7c8e45ac05eda9d6a1ea97f1c3624be
Japhet Secret <japyden@gmail.com>
2023-09-06 09:22:18 +0800

    Fixed color card issue

--- components/Skill/Card.vue
+++ components/Skill/Card.vue
@@ -6,13 +6,13 @@ defineProps({
 
 <template>
     <div class="cursor-pointer transition duration-150 ease-out transform hover:scale-110 my-4 lg:my-8">
-        <div :class="`bg-${raw_data.color}-600 shadow-lg rounded-lg text-center p-8 h-40`">
+        <div :class="`bg-rose-600 shadow-lg rounded-lg text-center p-8 h-40`">
             <img
                 alt="..."
                 class="shadow-md rounded-lg max-w-full w-16 h-14 mx-auto p-2 bg-white"
                 :src="`/media/about/skill-logos/${raw_data.src}`"
             />
-            <p class="text-lg text-white mt-4 font-semibold">{{ raw_data.title }}</p>
+            <p class="text-lg text-slate-900 mt-4 font-semibold">{{ raw_data.title }}</p>
         </div>
     </div>
 </template>
